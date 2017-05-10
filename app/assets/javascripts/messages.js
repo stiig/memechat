a410acb23f2ec341565bbdab516baffc6999eeca
stiig <vasiliyrps2@gmail.com>
2017-05-11 01:16:09 +0500

    clear textarea after send message

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,6 +2,7 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
     received: function (data) {
         var messages = $('.messages');
         messages.append(this.renderMessage(data));
+        $('#message_content').first().val('');
         messages.scrollTop(messages[0].scrollHeight);
     },
 
