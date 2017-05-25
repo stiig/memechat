91af3263d91073f0dc8fc0eb5a0676fa1e763376
stiig <vasiliyrps2@gmail.com>
2017-05-25 23:30:44 +0500

    add basic react components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,61 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const ChatBoxTitle = props => (
+    <h2>Your messages will be here</h2>
+)
+
+const ChatMessagesContainer = props => (
+    <div className="messages">
+        <ChatBoxTitle/>
+    </div>
+)
+
+const ChatFormSubmit = props => (
+    <input type="submit" name="commit" value="send" data-disable-with="send"/>
+)
+
+const ChatFormTextarea = props => (
+    <div>
+        <label htmlFor="message_your_message">
+            Your message: <textarea name="message[content]" id="message_content" defaultValue=''/>
+        </label>
+        <ChatFormSubmit/>
+    </div>
+)
+
+const ChatForm = props => (
+    <div>
+        <form action={props.action} acceptCharset="UTF-8" data-remote="true" method="post">
+            <input name="utf8" type="hidden" value="&#x2713;"/>
+            <ChatFormTextarea/>
+        </form>
+    </div>
+)
+
+const ChatBoxContainer = props => (
+    <div className="container">
+        <ChatMessagesContainer/>
+        <ChatForm action="/messages"/>
+    </div>
+)
+
+const ChatBoxWrapperContainer = props => (
+    <div className="wrapper">
+        <ChatBoxContainer/>
+    </div>
+)
+
+const ChatBox = props => (
+    <ChatBoxWrapperContainer/>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <ChatBox/>,
+        document.body.appendChild(document.createElement('div')),
+    )
+})
