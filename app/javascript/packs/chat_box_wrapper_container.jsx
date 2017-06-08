630e4dfc78099fba4433ac2a98a41ac254448f7f
stiig <vasiliyrps2@gmail.com>
2017-06-08 11:30:52 +0500

    moved styles for wrapper

--- app/javascript/packs/chat_box_wrapper_container.jsx
+++ app/javascript/packs/chat_box_wrapper_container.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ChatBoxContainer from './chat_box_container'
+import '../styles/chat_box_wrapper_container.scss'
 
 const ChatBoxWrapperContainer = props => (
     <div className="wrapper">
