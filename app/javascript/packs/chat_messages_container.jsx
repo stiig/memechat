6695bfdaec6bc19042b529356f38fe8e44bb271f
stiig <vasiliyrps2@gmail.com>
2017-06-09 11:27:37 +0500

    moved styles for messages

--- app/javascript/packs/chat_messages_container.jsx
+++ app/javascript/packs/chat_messages_container.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ChatBoxTitle from './chat_box_title'
+import '../styles/chat_messages_container.scss'
 
 const ChatMessagesContainer = props => (
     <div className="messages">
