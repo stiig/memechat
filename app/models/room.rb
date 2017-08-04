0aeff1accc543aeb8a26fe2db931b8268239da7a
stiig <vasiliyrps2@gmail.com>
2017-08-04 20:39:04 +0500

    add owner and messages to room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Room < ApplicationRecord
+  belongs_to :owner, foreign_key: :owner_id, class_name: 'User'
+  has_many :room_messages
+  has_many :messages, through: :room_messages
 end
