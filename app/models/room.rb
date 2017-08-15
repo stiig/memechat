5ab1b5b9424b56a3a3dbc08b58e725687e3a091c
stiig <vasiliyrps2@gmail.com>
2017-08-15 12:22:15 +0500

    rename back user to owner, remove room_participants

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Room < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, class_name: 'User'
+
   has_many :room_messages, dependent: :destroy
   has_many :messages, through: :room_messages
-  has_many :room_participants
 end
