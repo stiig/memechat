603e8ce6cfcfbacdfad4d817761a37b3ca5c9c84
stiig <vasiliyrps2@gmail.com>
2017-08-21 23:38:41 +0500

    add user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :messages
+  has_many :room_messages, class_name: 'Room::Message'
+  has_many :room_participants, class_name: 'Room::Participant'
 end
