9e8c315089a4fe3fc7336bd75fbb641941cd8552
stiig <vasiliyrps2@gmail.com>
2017-08-31 23:53:11 +0500

    moved relation up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one :user
   has_many :messages
   has_many :room_messages, class_name: 'Room::Message'
   has_many :room_participants, class_name: 'Room::Participant'
-  has_one :user
 end
