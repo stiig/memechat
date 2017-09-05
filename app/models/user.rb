bf62e85275696de93865611d43b96d9726eac96d
stiig <vasiliyrps2@gmail.com>
2017-09-05 20:36:21 +0500

    add notification to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_one :user
   has_many :messages
+  has_many :notifications
   has_many :room_messages, class_name: 'Room::Message'
   has_many :room_participants, class_name: 'Room::Participant'
 end
