c69a8db8629cb1a2d24656212d7bb15b27d148a5
stiig <vasiliyrps2@gmail.com>
2017-11-13 20:16:28 +0500

    remove unnecessary div

--- app/javascript/packs/chat_form.jsx
+++ app/javascript/packs/chat_form.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ChatFormTextarea from './chat_form_textarea'
 
 const ChatForm = props => (
-    <div>
-        <form action={props.action} acceptCharset="UTF-8" data-remote="true" method="post">
-            <input name="utf8" type="hidden" value="&#x2713;"/>
-            <ChatFormTextarea/>
-        </form>
-    </div>
+    <form action={props.action} acceptCharset="UTF-8" data-remote="true" method="post">
+        <input name="utf8" type="hidden" value="&#x2713;"/>
+        <ChatFormTextarea/>
+    </form>
 )
 
 export default ChatForm
