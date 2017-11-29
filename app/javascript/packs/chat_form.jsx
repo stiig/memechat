4881fca6dcf69f43da0e2a362e050fab33f531e4
stiig <vasiliyrps2@gmail.com>
2017-11-29 20:37:40 +0500

    pass handler

--- app/javascript/packs/chat_form.jsx
+++ app/javascript/packs/chat_form.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ChatFormTextarea from './chat_form_textarea'
 
 const ChatForm = props => (
-    <form action={props.action} acceptCharset="UTF-8" data-remote="true" method="post">
-        <input name="utf8" type="hidden" value="&#x2713;"/>
-        <ChatFormTextarea/>
-    </form>
+  <form action={props.action} acceptCharset="UTF-8" data-remote="true" method="post">
+    <input name="utf8" type="hidden" value="&#x2713;"/>
+    <ChatFormTextarea onSubmitHandler={props.onSubmitHandler}/>
+  </form>
 )
 
 export default ChatForm
