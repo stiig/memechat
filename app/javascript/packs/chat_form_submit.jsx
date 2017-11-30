b99595ea1a79de8919faf4eb8278bfed26f5401b
stiig <vasiliyrps2@gmail.com>
2017-11-30 20:54:57 +0500

    remove

--- app/javascript/packs/chat_form_submit.jsx
+++ app/javascript/packs/chat_form_submit.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const ChatFormSubmit = props => (
-    <input type="submit" name="commit" onSubmit={props.onSubmitHandler} value="send" data-disable-with="send"/>
-)
+const ChatFormSubmit = () => (
+  <input type="submit" name="commit" value="send"/>
+);
 
 export default ChatFormSubmit
