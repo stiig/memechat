ae6291ca43dec3bfd414b633f734dfca6e1b1791
stiig <vasiliyrps2@gmail.com>
2017-12-02 19:53:44 +0500

    pass value and handler to textarea

--- app/javascript/packs/chat_form_textarea.jsx
+++ app/javascript/packs/chat_form_textarea.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ChatFormSubmit from './chat_form_submit'
 
 const ChatFormTextarea = props => (
-    <div>
-        <label htmlFor="message_your_message">
-            Your message: <textarea name="message[content]" id="message_content" defaultValue=''/>
-        </label>
-        <ChatFormSubmit onSubmitHandler={props.onSubmitHandler}/>
-    </div>
+  <div>
+    <label htmlFor="message_your_message">
+      Your message: <textarea name="message[content]"
+                              id="message_content"
+                              value={props.value}
+                              onChange={props.onChangeHandler}
+                              />
+    </label>
+    <ChatFormSubmit/>
+  </div>
 )
 
 export default ChatFormTextarea
