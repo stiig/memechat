1307caa2452d5b7d8c7920682631f5914eda9667
stiig <vasiliyrps2@gmail.com>
2017-12-03 17:22:40 +0500

    extend chatform with react component

--- app/javascript/packs/chat_form.jsx
+++ app/javascript/packs/chat_form.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import ChatFormTextarea from './chat_form_textarea'
 
-const ChatForm = props => (
-  <form action={props.action} acceptCharset="UTF-8" data-remote="true" method="post">
-    <input name="utf8" type="hidden" value="&#x2713;"/>
-    <ChatFormTextarea onSubmitHandler={props.onSubmitHandler}/>
-  </form>
-)
+class ChatForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ""}
+  }
+
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+
+    const reqOptions = {
+      method: "POST",
+      body: form,
+      credentials: 'include',
+      headers: new Headers({'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content})
+    };
+
+    fetch(e.target.action, reqOptions)
+      .then(response => {
+        if (response.ok) {
+          this.setState({value: ""})
+        }
+      });
+  };
+
+  textAreaHandler = e => {
+    this.setState({value: e.target.value})
+  };
+
+  render() {
+    return <form action={this.props.action}
+                 onSubmit={this.onSubmitHandler}>
+      <input name="utf8"
+             type="hidden"
+             value="&#x2713;"/>
+      <ChatFormTextarea onChangeHandler={this.textAreaHandler} value={this.state.value}/>
+    </form>
+  }
+}
 
 export default ChatForm
