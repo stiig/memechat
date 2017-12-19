b7347b89dffeae6f7c6af225b0603525d87097ad
stiig <vasiliyrps2@gmail.com>
2017-12-19 20:19:04 +0500

    change button to material ui

--- app/javascript/packs/chat_form_submit.jsx
+++ app/javascript/packs/chat_form_submit.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Button from 'material-ui/Button';
 
 const ChatFormSubmit = () => (
-  <input type="submit" name="commit" value="send"/>
+  <Button color="primary" type="submit" name="commit" value="send">Отправить</Button>
 );
 
 export default ChatFormSubmit
