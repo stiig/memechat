68e30d19bb5c78eadaac5ac656de3fcae7600395
stiig <vasiliyrps2@gmail.com>
2017-12-20 14:59:13 +0500

    replace plain textarea on material text input

--- app/javascript/packs/chat_form_textarea.jsx
+++ app/javascript/packs/chat_form_textarea.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ChatFormSubmit from './chat_form_submit'
+import TextField from 'material-ui/TextField';
 
 const ChatFormTextarea = props => (
   <div>
     <label htmlFor="message_your_message">
-      Your message: <textarea name="message[content]"
+      Your message: <TextField name="message[content]"
                               id="message_content"
                               value={props.value}
                               onChange={props.onChangeHandler}
@@ -12,6 +13,6 @@ const ChatFormTextarea = props => (
     </label>
     <ChatFormSubmit/>
   </div>
-)
+);
 
 export default ChatFormTextarea
