ac36c38154c35beec42b1e08805e7eb516f127ef
stiig <vasiliyrps2@gmail.com>
2017-12-23 16:15:49 +0500

    fix indent

--- app/javascript/packs/chat_box_wrapper_container.jsx
+++ app/javascript/packs/chat_box_wrapper_container.jsx
@@ -3,9 +3,9 @@ import ChatBoxContainer from './chat_box_container'
 import '../styles/chat_box_wrapper_container.scss'
 
 const ChatBoxWrapperContainer = props => (
-    <div className="wrapper">
-        <ChatBoxContainer/>
-    </div>
+  <div className="wrapper">
+    <ChatBoxContainer/>
+  </div>
 )
 
 export default ChatBoxWrapperContainer
